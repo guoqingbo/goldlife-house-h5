f31bf14a21a06757e9e05c3ab8b24843a525d136
guo.qingbo <guoqingbo@cd121.com>
2018-07-16 19:11:04 +0800

    ff

--- src/page/myCare/myCare.vue
+++ src/page/myCare/myCare.vue
@@ -1,12 +1,14 @@
 <template>
   <!--房源结果列表-->
   <div class="">
+    <!--顶部头-->
+    <head-top goBack="true"/>
     <!--头部导航-->
-    <div >
+    <div class="header-nav">
       <!--返回图标-->
       <span></span>
       <!--导航项-->
-      <ul>
+      <ul class="nav-item">
         <li>租房</li>
         <li>二手房</li>
         <li>楼盘</li>
@@ -19,6 +21,9 @@
   </div>
 </template>
 <script>
+  import api from '../../api/axios'
+  import headTop from '../../components/header/head';
+
   export default {
     props:[],
     data(){
@@ -26,9 +31,18 @@
         selected:""
       }
     },
-
+    components: {
+      headTop,
+    },
   }
 </script>
 <style lang="scss" scoped>
-
+ .header-nav{
+   .nav-item{
+     display: flex;
+     li{
+       flex:1 1;
+     }
+   }
+ }
 </style>
