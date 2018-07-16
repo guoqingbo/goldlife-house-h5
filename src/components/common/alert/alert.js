14501ae5c23dc959eb4d9af76e6016d844dbc3b1
guo.qingbo <guoqingbo@cd121.com>
2018-07-16 11:03:11 +0800

    ff

--- src/components/common/alert/alert.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import alert from './alert.vue';
-const AlertConstructor = Vue.extend(alert);
-const alertInstance = new AlertConstructor();
-document.body.appendChild(alertInstance.$mount().$el)
-export default alertInstance;
