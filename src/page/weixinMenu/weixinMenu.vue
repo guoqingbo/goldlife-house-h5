87e84d3d4ca10a6c8f756789ae0f249ea7ea479f
guo.qingbo <guoqingbo@cd121.com>
2018-07-17 10:43:18 +0800

    ff

--- src/page/weixinMenu/weixinMenu.vue
+++ src/page/weixinMenu/weixinMenu.vue
@@ -6,6 +6,7 @@
 
 <script>
   import api from '../../api/axios' //后台数据接口
+  import { MessageBox } from 'mint-ui';
   export default {
 
     created(){
@@ -14,13 +15,18 @@
     methods: {
       // 搜索结果展示
       getWeixinMenu(){
-        alert(JSON.stringify(this.$route))
+//        this.$toast({
+//          message: JSON.stringify(this.$route.fullPath),
+//          position: 'bottom',
+//          duration: 0
+//        })
           let Params = {
             redirectType:this.$route.params.redirectType,// 可能值 house  account fund finan loan invite
             code:this.$route.query?this.$route.query.code:"",
           };
-        alert(this.$route.query);
-        alert(JSON.stringify(this.Params));
+        MessageBox.alert(this.$route.fullPath,"fullPath");
+//        MessageBox.alert(this.$route.query,"qurey");
+//        MessageBox.alert(this.$route.params,"params");
         api.getWeixinMenu(Params)
           .then(res=>{
               console.log(res.data)
