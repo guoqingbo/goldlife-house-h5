9e2fc192be201df0316842609b994e4bfb1bbc42
li.lei <1419347845@qq.com>
2018-07-18 16:43:11 +0800

    客户看房

--- src/router/index.js
+++ src/router/index.js
@@ -199,18 +199,16 @@ const routes = [
     component: mapIncrease,
   },
   {
-<<<<<<< Updated upstream
     path: '/user/weixin/menu',//可能值 house  account fund finan loan invite
     name: 'weixinMenu',//获取微信菜单
     meta: { redirect: true},
   },
-=======
+    {
     path: '/villageDetail',
     name: 'villageDetail',
     component: villageDetail,
   },
 
->>>>>>> Stashed changes
 ];
 
 
