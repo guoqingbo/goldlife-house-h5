caa9a0accf7a9387b4465bce0f7d32459251d0ed
li.lei <1419347845@qq.com>
2018-07-31 14:14:31 +0800

    租房回退修改

--- src/page/houseDetail/houseBuyDetail.vue
+++ src/page/houseDetail/houseBuyDetail.vue
@@ -473,12 +473,13 @@ console.log(this.$store.state.activeInfo)
       getHomeDetail(data) {
         if(data == this.houseId){
           //window.reload();
-          this.$router.go(0);
+          //this.$router.go(0);
         }else{
           this.houseId = data;
           this.houseType = '1';
           this.getHouseDetail();
         }
+
         this.menu();
       },
       getCompareNum() {
